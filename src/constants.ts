6dd6184c7f916457fe9654458279511caea4ba47
Yuki <yuki@localhost>
2022-11-10 02:32:58 +0900

    Make timestamp include seconds

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const DATA_FILE = "data.json";
 export const TARGET_USER_ID = "1589208744592044033";
-export const TWEET_DATE_FORMAT = "MM/dd HH:mm";
+export const TWEET_DATE_FORMAT = "MM/dd HH:mm:ss";
 export const FETCH_INTERVAL = 15000;
